a1defafa0d7ed8497187f5de018d578bdea6f7da
Kiran <kiranbabu3355@gmail.com>
2025-05-21 14:53:15 +0530

    feat: add AbstractEntity to data source entities

--- db/data-source.ts
+++ db/data-source.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm'
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import Employee from '../entities/employee.entity'
 import Address from '../entities/address.entity'
+import AbstractEntity from '../entities/abstract.entity'
 
 const datasource=new DataSource({
     type:"postgres",
@@ -15,7 +16,7 @@ const datasource=new DataSource({
     synchronize: false,
     logging:true,
     namingStrategy:new SnakeNamingStrategy,
-    entities: [Employee,Address],
+    entities: [Employee,Address,AbstractEntity],
     migrations:["dist/db/migrations/*.js"]
 })
 
